a33fe9295cbbd5ed7fb858e6d883822db77f0e1d
FeudHawk <78530338+FeudHawk@users.noreply.github.com>
2023-10-19 13:43:37 +0200

    Lille fix

--- SSO/client.py
+++ SSO/client.py
@@ -1,9 +1,9 @@
 from socket import*
 
-server_name = input("Input IP adress of server\n>")
-server_port = 12000
-client_socket = socket(AF_INET, SOCK_DGRAM)
+server_name = '192.168.215.83'
+server_port = 12345
+clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 while True:
     message = input("Write message to send\n>")
-    client_socket.sendto(message.encode(), (server_name, server_port))
+    clientSocket.sendto(message.encode(), (server_name, server_port))
