df1168e63b93a876153990970f9a19756c99f1e8
FeudHawk <78530338+FeudHawk@users.noreply.github.com>
2023-10-23 10:43:28 +0200

    Update Krav1.py

--- TraeningsModul/Krav1.py
+++ TraeningsModul/Krav1.py
@@ -14,7 +14,6 @@ gps = GPS_Minimum(uart)
 
 bat_adc = ADC(Pin(34))
 bat_adc.atten(ADC.ATTN_11DB)
-bat_scaling = 4.2 / 2245 # Volt/ADC Værdi
 
 def get_gpsdata():
     speed = lat = lon = None
@@ -30,17 +29,12 @@ def get_gpsdata():
     else:
         return False
 
-#Kode tager et gennemsnit af adc værdien, Kode taget fra Bo Hansen, twoway_remote_data.py
-def read_battery_voltage_avg64():      
+#Kode tager et gennemsnit af adc værdien og omregner til procent
+#Kode inspiration taget fra Bo Hansen, twoway_remote_data.py   
+def get_battery_percent():
     adc_val = 0
     for i in range(64):
-        adc_val += bat_adc.read()      
-    voltage = bat_scaling * (adc_val >> 6) # >> fast divide by 64
-    adc2 = adc_val >> 6
-    return adc2
-    return voltage
-
-def get_battery_percent():
-    adc64_val = read_battery_voltage_avg64()
-    bat_percent = (adc64_val - 1500) / 7.3
+        adc_val += bat_adc.read() #lægger 64 adc læsninger sammen
+    adc64_val = adc_val >> 6 #dividere med 64
+    bat_percent = (adc64_val - 1500) / 7.3 # ADC1500 = 0% og 1% = ADC7.3
     return bat_percent
