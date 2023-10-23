683ca68d4c9b4a5ecac3ac9a233063280a103139
Shadow02Hunter <fragomeni02@gmail.com>
2023-10-23 14:03:30 +0200

    Krav1 GPS fix

--- TraeningsModul/Krav1.py
+++ TraeningsModul/Krav1.py
@@ -1,10 +1,10 @@
 #Krav: Løsningen skal som minimum kunne fremvise lokation og batteridata 
 #via et online dashboard, med en opdateringsrate på minimum 2 opdateringer i minuttet.
 
-import umqtt_robust2 as mqtt
+import umqtt_robust2 as mqtt #Dette biblotek henter credentials, som gør det muligt at forbinde ESP32 til internet og Adafruit.
 from machine import UART, ADC, Pin
-from time import sleep
-from gps_bare_minimum import GPS_Minimum
+from time import sleep #henter tids bibloteket, som gør det muligt at bruge sleep funktionen.
+from gps_bare_minimum import GPS_Minimum #Dette biblotek henter tid og dat, derudover henter den kordinat system til jordkloden, som gør det muligt at indhente latitude, longtitude og speed værdier.
 
 gps_port = 2 #Det er hardware UART som har tildelt rx 16 og tx 17 på esp32
 gps_dataspeed = 9600 #Det er kommunikationshastighed i bits per sekund. 
@@ -16,16 +16,18 @@ bat_adc = ADC(Pin(34))
 bat_adc.atten(ADC.ATTN_11DB)
 
 def get_adafruit_gps():
-    speed, lat, lon = "", "", ""
+    speed, lat, lon = "", "", ""  # Initialiser variablerne
     
-    if gps.recieve_nmea_data(): #nmea dataen er speed, latitude og longitude
-        if gps.get_speed() != -999 and gps.get_latitude() != -999.0 and gps.get_longitude() != -999.0 and gps.get_validity() == "A":
-            speed = str(gps.get_speed()) #definere speed
-            lat = str(gps.get_latitude()) #definere latitude
-            lon = str(gps.get_longitude()) #definere longtitude
-            return speed + "," + lat + "," + lon + "," + "0.0" #0.0 er altitude, adafruit skal bruge værdien selvom vi ikke har den
-        else:
-            print(f"Venter på GPS DATA - Gå hen til et sted med bedre signal:\nspeed: {speed}\nlatitude: {lat}\nlongtitude: {lon}") #Hvis gps ikke modtager andet data end -999.0 printer den find bedre lokation
+    if gps.receive_nmea_data():
+        # hvis der er kommet end bruggbar værdi på alle der skal anvendes
+        if gps.get_speed() != 0 and gps.get_latitude() != -999.0 and gps.get_longitude() != -999.0:
+            # returnerer data med adafruit gps format
+            speed =str(gps.get_speed())
+            lat = str(gps.get_latitude())
+            lon = str(gps.get_longitude())
+            return speed + "," + lat + "," + lon + "," + "0.0"
+        else: # hvis ikke både hastighed, latitude og longtitude er korrekte 
+            print(f"Waiting for GPS DATA - Move GPS to place with acces to the sky:\nspeed: {speed}\nlatitude: {lat}\nlongtitude: {lon}")
             return False
     else:
         return False
