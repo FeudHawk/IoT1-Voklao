217cf87cb27f7a62a48d8774b28566dd1ecc8bca
Shadow02Hunter <fragomeni02@gmail.com>
2023-10-24 11:36:54 +0200

    Fik koden til at virke

--- TraeningsModul/neop.py
+++ TraeningsModul/neop.py
@@ -12,7 +12,20 @@ def set_color(r, g, b):
     np.write()
 
 while True:
-    if mqtt.besked == "gult kort":
-        set_color(0, 255, 255)
+    try:
+    
+        if mqtt.besked == "gult kort":
+            set_color(255, 255, 0)
+        
+        if len(mqtt.besked) != 0: # Her nulstilles indkommende beskeder
+            mqtt.besked = ""
             
+        mqtt.sync_with_adafruitIO() # igangsæt at sende og modtage data med Adafruit IO             
+        #sleep(1) # Udkommentér denne og næste linje for at se visuelt output
+        #print(".", end = '') # printer et punktum til shell, uden et enter        
+    
+    except KeyboardInterrupt: # Stopper programmet når der trykkes Ctrl + c
+        print('Ctrl-C pressed...exiting')
+        mqtt.c.disconnect()
+        mqtt.sys.exit()
             
