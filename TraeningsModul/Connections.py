8d24b49f96ed9e11fb7c9740e26e2be3a7e194ba
FeudHawk <78530338+FeudHawk@users.noreply.github.com>
2023-10-26 12:34:49 +0200

    Create Connections.py

---
+++ TraeningsModul/Connections.py
@@ -0,0 +1,18 @@
+#Her er en liste af de forbindelser der er på vores TM
+
+Neopixel = 26
+
+Display1_CLK = 22
+Display1_DIO = 23
+
+Display2_CLK = 32
+Display2_DIO = 33
+
+GPS_PPS = 5
+GPS_Rx = 17
+GPS_Tx = 16
+
+BAT_Vol = 34
+
+MPU_SCL = 18
+MPU_SDA = 19
