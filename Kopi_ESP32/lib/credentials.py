2ac2c48d09f81382f353ce0985504dee59d0ed39
FeudHawk <78530338+FeudHawk@users.noreply.github.com>
2023-11-05 17:02:27 +0100

    Update credentials.py

--- Kopi_ESP32/lib/credentials.py
+++ Kopi_ESP32/lib/credentials.py
@@ -2,7 +2,7 @@ credentials = {
     'ssid' : 'LTE-1824',
     'password' : 'Password1',
     'ADAFRUIT_IO_URL' : b'io.adafruit.com',
-    'ADAFRUIT_USERNAME' : b'Shadow02Hunter',
-    'ADAFRUIT_IO_KEY' : b'aio_pASe02kCLIJV6KBAfeyRPhEP1nF9',
+    'ADAFRUIT_USERNAME' : b'',
+    'ADAFRUIT_IO_KEY' : b'',
     'ADAFRUIT_IO_FEEDNAME' : b'NeoPixel'
     }
